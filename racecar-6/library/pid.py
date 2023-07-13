06e99da1254813a20092036fc652f46d44cf117e
Nathaniel <nathutesfamicael@gmail.com>
2023-07-13 16:08:42 -0400

    PIDController

--- racecar-6/library/pid.py
+++ racecar-6/library/pid.py
@@ -1,2 +1,33 @@
-class PID:
-    pass
+import time
+
+
+class PIDController:
+    def __init__(self, p, i, d) -> None:
+        self.k_p = p
+        self.k_i = i
+        self.k_d = d
+        self.last_time=time.perf_counter()
+        self.setpoint=0
+        self.error=0
+        self.sum=0
+
+    def set_setpoint(self,setpoint):
+        self.setpoint=setpoint
+    
+    def calculate(self,position):
+        error=self.setpoint-position
+        current_time=time.perf_counter()
+        dt=current_time-self.last_time
+        self.last_time=current_time
+
+        derivative=(error-self.error)/dt
+        self.sum+=dt*error
+
+        return self.k_p*error+self.k_i+self.sum +self.k_d*derivative
+
+
+
+
+
+
+        
