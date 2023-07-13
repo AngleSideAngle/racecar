3f64a843e2b48bbc327510e0da908321aa311f8a
Nathaniel <nathutesfamicael@gmail.com>
2023-07-13 16:18:39 -0400

    PIDController

--- racecar-6/library/pid.py
+++ racecar-6/library/pid.py
@@ -1,29 +1,36 @@
 import time
-
+from typing import Optional
 
 class PIDController:
-    def __init__(self, p, i, d) -> None:
+    def __init__(self, p: float, i: float, d: float) -> None:
         self.k_p = p
         self.k_i = i
         self.k_d = d
-        self.last_time=time.perf_counter()
-        self.setpoint=0
-        self.error=0
-        self.sum=0
+        self.last_time = time.perf_counter()
+        self.setpoint = 0
+        self.error = 0
+        self.sum = 0
 
-    def set_setpoint(self,setpoint):
-        self.setpoint=setpoint
+    def set_setpoint(self, setpoint: float) -> None:
+        self.setpoint = setpoint
     
-    def calculate(self,position):
-        error=self.setpoint-position
-        current_time=time.perf_counter()
-        dt=current_time-self.last_time
-        self.last_time=current_time
+    def calculate(
+        self,
+        position: float,
+        setpoint: Optional[float] = None
+    ) -> float:
+        if setpoint:
+            self.setpoint = setpoint
 
-        derivative=(error-self.error)/dt
-        self.sum+=dt*error
+        error = self.setpoint-position
+        current_time = time.perf_counter()
+        dt = current_time-self.last_time
+        self.last_time = current_time
 
-        return self.k_p*error+self.k_i+self.sum +self.k_d*derivative
+        derivative = (error-self.error) / dt
+        self.sum += dt * error
+
+        return self.k_p * error + self.k_i + self.sum + self.k_d * derivative
 
 
 
