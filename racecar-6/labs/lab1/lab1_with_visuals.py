0dffa7e689320c4209873190272974a87445cb48
Asa Paparo <asapaparo@gmail.com>
2023-07-13 10:47:47 -0400

    finished rewriting lab1_with_visuals with command based

--- racecar-6/labs/lab1/lab1_with_visuals.py
+++ racecar-6/labs/lab1/lab1_with_visuals.py
@@ -14,8 +14,6 @@ import sys
 import math
 import numpy as np
 
-
-
 sys.path.insert(1, "../../library")
 import racecar_core
 import racecar_utils as rc_utils
@@ -36,9 +34,6 @@ rc = racecar_core.create_racecar()
 global scheduler
 scheduler: commands.Scheduler = commands.Scheduler(default_command=commands.run(rc.drive.stop))
 
-global queue
-queue = []
-
 global location
 location = [0,0]
 global angle
@@ -51,23 +46,32 @@ global loc_over_time
 loc_over_time = []
 loc_over_time.append(location)
 
-global counter
-counter = 0
-
 def drive_command(time: float, power: float, angle: float) -> commands.Command:
-    return commands.run(lambda: rc.drive.set_speed_angle(power, angle)).with_timeout(time)
+    return commands.run(lambda: rc.drive.set_speed_angle(power, angle)).and_then(commands.run(rc.drive.stop)).with_timeout(time)
 
 def square() -> commands.Command:
-    side = commands.Sequence(
-        drive_command(2, 0.5, 0),
+    side = lambda: commands.Sequence(
+        drive_command(1, 0.5, 0),
         drive_command(0.5, 0.5, 1)
     )
-    return commands.Sequence(*[side for _ in range(5)]).along_with(commands.print("Driving in a square..."))
+    return commands.Sequence(*[side() for _ in range(4)]).along_with(commands.print_once("Driving in a square..."))
+
+def star() -> commands.Command:
+    side = lambda: commands.Sequence(
+        drive_command(5, 1, 0),
+        drive_command(1, 1, 1),
+        drive_command(3, -1, -1)
+    )
+    return commands.Sequence(*[side() for _ in range(5)]).along_with(commands.print_once("Driving in a star..."))
+
+def circle() -> commands.Command:
+    return drive_command(5.5, 1, 1).along_with(commands.print_once("Driving in a circle..."))
 
 def start():
     """
     This function is run once every time the start button is pressed
     """
+    a = circle()
     # Begin at a full stop
     rc.drive.stop()
 
@@ -92,15 +96,10 @@ def update():
     is pressed
     """
     # TODO (warmup): Implement acceleration and steering
-    rc.drive.set_speed_angle(0, 0)
     global angle
     global location
     global velocity
     global loc_over_time
-    global counter
-    global scheduler
-    
-
 
     accel = rc.physics.get_linear_acceleration()[2] 
     velocity += accel if accel > 0.05 or accel < -0.05 else 0
@@ -109,23 +108,12 @@ def update():
     location[1] += math.sin(math.radians(angle)) * velocity * rc.get_delta_time()
     loc_over_time.append(location.copy())
     angle -= math.degrees(rc.physics.get_angular_velocity()[1]) * rc.get_delta_time() 
-
-    if rc.controller.was_pressed(rc.controller.Button.X):
-        scheduler.schedule(drive_command(1, 0.5, -0.2))
     
     if rc.controller.was_pressed(rc.controller.Button.A):
-        print("Driving in a circle...")
-        queue.append([5.5,1,1])
+        scheduler.schedule(circle())
 
     if rc.controller.was_pressed(rc.controller.Button.Y):
-        print("Driving in a star...")
-        side = commands.Sequence(
-            drive_command(5, 1, 0),
-            drive_command(1, 1, 1),
-            drive_command(3, -1, -1)
-        )
-        star = commands.Sequence(*[side for _ in range(5)])
-        scheduler.schedule(star)
+        scheduler.schedule(star())
 
     if rc.controller.was_pressed(rc.controller.Button.B):
         scheduler.schedule(square())
@@ -136,26 +124,6 @@ def update():
 
     # TODO (main challenge): Drive in a shape of your choice when the Y button
     # is pressed
-
-     
-    if len(queue) != 0:
-        # if queue[0][0] == "rotrel":
-        #     start_angular_vel = rc.physics.get_angular_velocity()
-        #     queue.pop(0)
-        # else:
-            queue[0][0] -= rc.get_delta_time()
-            command = queue[0]
-
-            rc.drive.set_speed_angle(command[1], command[2])
-            if command[0] <= 0:
-                x = np.array(loc_over_time) 
-                print(x)
-                plt.scatter(x[:, 0], x[:, 1])
-                plt.savefig('foo.png')
-                velocity = 0
-                loc_over_time = []
-                loc_over_time.append(location)
-                queue.pop(0)
 
     scheduler.run()
             
