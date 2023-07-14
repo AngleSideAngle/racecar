dcfe1813c1c12deca5d804f807d657624668a234
Asa Paparo <asapaparo@gmail.com>
2023-07-13 23:43:52 -0400

    idk

--- racecar-6/library/pid.py
+++ racecar-6/library/pid.py
@@ -1,40 +1,46 @@
 import time
 from typing import Optional
 
+
 class PIDController:
-    def __init__(self, p: float, i: float, d: float) -> None:
-        self.k_p = p
-        self.k_i = i
-        self.k_d = d
-        self.last_time = time.perf_counter()
+    """
+    A PID implementation with variable time intervals, using system time
+    """
+
+    def __init__(self, k_p: float, k_i: float, k_d: float) -> None:
+        self.k_p = k_p
+        self.k_i = k_i
+        self.k_d = k_d
+
+        self.prev_time = time.perf_counter()
+        self.prev_error = 0
         self.setpoint = 0
-        self.error = 0
         self.sum = 0
 
-    def set_setpoint(self, setpoint: float) -> None:
-        self.setpoint = setpoint
-    
     def calculate(
         self,
         position: float,
         setpoint: Optional[float] = None
     ) -> float:
+        """
+        Calculates control output to minimize error based on the PID algorithm
+        """
+
         if setpoint:
             self.setpoint = setpoint
 
-        error = self.setpoint-position
-        current_time = time.perf_counter()
-        dt = current_time-self.last_time
-        self.last_time = current_time
+        error = self.setpoint - position
 
-        derivative = (error-self.error) / dt
+        current_time = time.perf_counter()
+        dt = current_time - self.prev_time
+        self.prev_time = current_time
+
+        derivative = (error - self.prev_error) / dt
         self.sum += dt * error
+
+        self.prev_error = error
 
         return self.k_p * error + self.k_i + self.sum + self.k_d * derivative
 
-
-
-
-
-
-        
+    def __repr__(self) -> str:
+        return f"PID: k_p: {self.k_p}, k_i: {self.k_i}, k_d: {self.k_d}, setpoint: {self.setpoint}, error: {self.prev_error}, integral: {self.sum}"
