8c934762c1608f5aedf70db27247d9cb0116c2f0
Asa Paparo <asapaparo@gmail.com>
2023-07-19 14:02:53 -0400

    colors

--- racecar-6/labs/p1challenge/p1challenge.py
+++ racecar-6/labs/p1challenge/p1challenge.py
@@ -13,10 +13,35 @@ Phase 1 Challenge - Cone Slaloming
 import sys
 import cv2 as cv
 import numpy as np
+from enum import Enum
 
 sys.path.insert(0, "../../library")
 import racecar_core
 import racecar_utils as rc_utils
+
+########################################################################################
+# State
+########################################################################################
+
+class State(Enum):
+    LINE_FOLLOWING = 0
+    CONE_SLALOM = 1
+    PARKED = 2
+
+class Color(Enum, tuple[tuple[int, int, int], tuple[int, int, int]]):
+
+    # Line colors
+    YELLOW = ((0, 0, 0), (0, 0, 0))
+    BLUE = ((91-20, 106-45, 206-30), (91+20, 110+45, 208+40))
+    GREEN = ((56-30, 66-10, 179-60), (61+30, 100+30, 173+40))
+
+    # Cone colors
+    ORANGE = ((0, 0, 0), (0, 0, 0))
+    PURPLE = ((0, 0, 0), (0, 0, 0))
+
+    # Both
+    RED = ((0, 0, 0), (0, 0, 0))
+
 
 ########################################################################################
 # Global variables
@@ -25,6 +50,10 @@ import racecar_utils as rc_utils
 rc = racecar_core.create_racecar()
 
 # Add any global variables here
+
+# The smallest contour we will recognize as a valid contour
+MIN_CONTOUR_AREA = 60
+
 
 ########################################################################################
 # Functions
